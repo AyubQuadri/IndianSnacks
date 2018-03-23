9ace1b9e3c75701c1c3fbfaa2161efea759dbce9
Ayub Quadri <ayub.quadri@cyient-insights.com>
2018-03-23 11:13:32 +0530

    CNN Architecture using Keras Tensorflow Code

--- IndianSnacksdatasetand code/CNNclassifier.py
+++ IndianSnacksdatasetand code/CNNclassifier.py
@@ -1,97 +1,157 @@
 import cv2                 # working with, mainly resizing, images
 import numpy as np         # dealing with arrays
-import os                  # dealing with directories
 from random import shuffle # mixing up or currently ordered data that might lead our network astray in training.
 from tqdm import tqdm
-from scipy import misc
-import tflearn
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
-import tensorflow as tf
-import matplotlib.pyplot as plt
+
+from timeit import default_timer as timer
+import keras
+from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.models import model_from_json
+from keras.utils import np_utils
+import numpy as np
+from matplotlib import pyplot as plt
 import indiansnacksdataset as data
-tf.reset_default_graph()
-TRAIN_DIR ='train_images'
-TEST_DIR ='test_images'
-IMG_SIZE = 280
-LR = 1e-3
-#MODEL_NAME = 'quickest.model'.format(LR, '2conv-basic')
 
-
-train_data= data.load_train_data()
-train = train_data[:-200]
-test = train_data[-200:]
-
-X = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
-Y = [i[1] for i in train]
-test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
-test_y = [i[1] for i in test]
-X = X.reshape([-1, 280, 280, 1])
-test_x = test_x.reshape([-1, 280, 280, 1])
-
-convnet = input_data(shape=[None, 280, 280, 1], name='input')
-
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 128, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = fully_connected(convnet, 1024, activation='relu')
-convnet = dropout(convnet, 0.8)
-
-convnet = fully_connected(convnet, 10, activation='softmax')
-convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
-
-
-model = tflearn.DNN(convnet)
-
-
-
-
-model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}) , show_metric=True)
-
-
+#train_data = data.load_train_data()
 test_data = data.load_test_data()
 
-fig=plt.figure()
+#train_data = np.load('train_data_K.npy')
+#test_data = np.load('test_data_k.npy')
+batch_size = 128
+num_classes = 10
+epochs = 10
+IMG_SIZE =280
 
-for num,data in enumerate(test_data[:12]):
+# input image dimensions
 
-    
-    img_num = data[1]
-    img_data = data[0]
-    y = fig.add_subplot(3,4,num+1)
-    orig = img_data
-    data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
-    #model_out = model.predict([data])[0]
-    model_out = model.predict([data])[0]
-    
-    if np.argmax(model_out) == 0: str_label='samosa'
-    elif np.argmax(model_out) == 1: str_label='kachori'
-    elif np.argmax(model_out) == 2: str_label='aloo_paratha'
-    elif np.argmax(model_out) == 3: str_label='idli'
-    elif np.argmax(model_out) == 4: str_label='jalebi'
-    elif np.argmax(model_out) == 5: str_label='tea'
-    elif np.argmax(model_out) == 6: str_label='paneer_tikka'
-    elif np.argmax(model_out) == 7: str_label='dosa'
-    elif np.argmax(model_out) == 8: str_label='omlet'
-    elif np.argmax(model_out) == 9: str_label='poha'
-        
-    y.imshow(orig,cmap='gray')
-    plt.title(str_label)
-    y.axes.get_xaxis().set_visible(False)
-    y.axes.get_yaxis().set_visible(False)
+# load train-data 
+
+start = timer()
+
+train = test_data[:-200]
+val = test_data[-200:]
+
+X_train = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y_train = [i[1] for i in train]
+
+
+val_x = np.array([i[0] for i in val]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+val_y = [i[1] for i in val]
+
+
+
+
+#plt.imshow(X_train[0].shape)
+X_train[0].shape
+
+input_shape = (IMG_SIZE, IMG_SIZE, 1)
+
+# convert the data to the right type
+X_train = X_train.astype('float32')
+val_x = val_x.astype('float32')
+X_train /= 255
+val_x /= 255
+print('x_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(val_x.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices - this is for use in the
+# categorical_crossentropy loss below
+Y_train = np_utils.to_categorical(Y_train, num_classes)
+val_y = np_utils.to_categorical(val_y, num_classes)
+
+
+modelCNN = Sequential()
+modelCNN.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation='relu', input_shape=input_shape))
+modelCNN.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+modelCNN.add(Conv2D(64, (5, 5), activation='relu'))
+modelCNN.add(MaxPooling2D(pool_size=(2, 2)))
+
+modelCNN.add(Conv2D(64, (5, 5), activation='relu'))
+modelCNN.add(MaxPooling2D(pool_size=(2, 2)))
+
+modelCNN.add(Conv2D(128, (5, 5), activation='relu'))
+modelCNN.add(MaxPooling2D(pool_size=(2, 2)))
+
+modelCNN.add(Conv2D(64, (5, 5), activation='relu'))
+modelCNN.add(MaxPooling2D(pool_size=(2, 2)))
+
+modelCNN.add(Flatten())
+
+modelCNN.add(Dense(1024, activation='relu'))
+modelCNN.add(Dense(num_classes, activation='softmax'))
+
+modelCNN.summary()
+
+modelCNN.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+
+
+history = modelCNN.fit(X_train, Y_train, 
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data= (val_x,val_y))
+
+score = modelCNN.evaluate(val_x, val_y, verbose=0)
+
+
+end = timer()
+print(end - start)
+
+print(history.history['acc'])
+print(history.history['val_acc'])
+
+print(history.history['loss'])
+print(history.history['val_loss'])
+
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 
+
+# save the model in Saved_Model Folder
+ 
+# serialize model to JSON
+model_json = modelCNN.to_json()
+with open("Saved_Model/model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("Saved_Model/model.h5")
+print("Saved model to disk")
+ 
+# later...
+ 
+# load json and create model
+json_file = open('Saved_Model/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("Saved_Model/CNNModelIndian.h5")
+print("Loaded model from disk")
+
+#predict using the test model
+data.predict_test(loaded_model, 12)
 
--- IndianSnacksdatasetand code/indiansnacksdataset.py
+++ IndianSnacksdatasetand code/indiansnacksdataset.py
@@ -8,16 +8,16 @@ TEST_DIR= 'test_images'
 IMG_SIZE=280
 def label_img(img):
     word = img.split("(")[-2]
-    if word=='samosa ': return [1,0,0,0,0,0,0,0,0,0]
-    elif word=='kachori ': return [0,1,0,0,0,0,0,0,0,0]
-    elif word=='aloo_paratha ': return [0,0,1,0,0,0,0,0,0,0]
-    elif word=='idli ': return [0,0,0,1,0,0,0,0,0,0]
-    elif word=='jalebi ': return [0,0,0,0,1,0,0,0,0,0]
-    elif word=='tea ': return [0,0,0,0,0,1,0,0,0,0]
-    elif word=='paneer_tikka ': return [0,0,0,0,0,0,1,0,0,0]
-    elif word=='dosa ': return [0,0,0,0,0,0,0,1,0,0]
-    elif word=='omlet ': return [0,0,0,0,0,0,0,0,1,0]
-    elif word=='poha ': return [0,0,0,0,0,0,0,0,0,1]
+    if word=='samosa ': return 0
+    elif word=='kachori ': return 1
+    elif word=='aloo_paratha ': return 2
+    elif word=='idli ': return 3
+    elif word=='jalebi ': return 4
+    elif word=='tea ': return 5
+    elif word=='paneer_tikka ': return 6
+    elif word=='dosa ': return 7
+    elif word=='omlet ': return 8
+    elif word=='poha ': return 9
     
 def load_train_data():
     training_data = []
@@ -28,7 +28,7 @@ def load_train_data():
         img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
         training_data.append([np.array(img),np.array(label)])
     shuffle(training_data)
-    np.save('train_data.npy', training_data)
+    np.save('train_data_k.npy', training_data)
     return training_data
 
 def load_test_data():
@@ -40,7 +40,7 @@ def load_test_data():
         img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
         testing_data.append([np.array(img),np.array(label)])
     shuffle(testing_data)
-    np.save('train_data.npy', testing_data)
+    np.save('test_data_k.npy', testing_data)
     return testing_data
 
 def load_train_data_NPY():
@@ -50,3 +50,34 @@ def load_train_data_NPY():
 def load_test_data_NPY():
     data = np.load('test_data.npy')
     return data
+
+
+def predict_imgs(model,No_of_Images):
+    fig=plt.figure()
+    for num,data in enumerate(train[:No_of_Images]):
+
+        
+        img_num = data[1]
+        img_data = data[0]
+        y = fig.add_subplot(3,No_of_Images/3,num+1)
+        orig = img_data
+        data = img_data.reshape(-1,IMG_SIZE,IMG_SIZE,1)
+        #model_out = model.predict([data])[0]
+        model_out = model.predict([data])[0]
+        
+        if np.argmax(model_out) == 0: str_label='samosa'
+        elif np.argmax(model_out) == 1: str_label='kachori'
+        elif np.argmax(model_out) == 2: str_label='aloo_paratha'
+        elif np.argmax(model_out) == 3: str_label='idli'
+        elif np.argmax(model_out) == 4: str_label='jalebi'
+        elif np.argmax(model_out) == 5: str_label='tea'
+        elif np.argmax(model_out) == 6: str_label='paneer_tikka'
+        elif np.argmax(model_out) == 7: str_label='dosa'
+        elif np.argmax(model_out) == 8: str_label='omlet'
+        elif np.argmax(model_out) == 9: str_label='poha'
+            
+        y.imshow(orig,cmap='gray')
+        plt.title(str_label)
+        y.axes.get_xaxis().set_visible(False)
+        y.axes.get_yaxis().set_visible(False)
+    plt.show()
